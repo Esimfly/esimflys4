80267012abb6f3797d455d5a0bd5fe052d5ba9e6
Esimfly <esimfly@outlook.com>
2025-05-29 21:59:28 +0300

    Update check.js

--- pages/api/check.js
+++ pages/api/check.js
@@ -87,7 +87,6 @@ export default async function handler(req, res) {
     res.status(200).json({
       summary: {
         totalAvailableBalance: `${totalAvailableBalanceSum.toFixed(2)} GB`,
-        totalActivatedBalance: `${totalActivatedBalanceSum.toFixed(2)} GB`,
       },
       customer: customers[0],
     });
